4da923378e30cab3cde9aaa3e6bd1951ceebaf83
Kirillkriv <kirill.krivoy@icloud.com>
2025-07-16 13:48:44 +0500

    feat - implemented s3 storage adapter

--- services/bot/adapters/adapters.py
+++ services/bot/adapters/adapters.py
@@ -1,8 +1,17 @@
+from io import BytesIO
 from json import loads
-from typing import Callable, Dict
+from typing import Callable, Dict, Optional
 
-from ..domain.ports.output import LocalePort, KeyboardProviderPort
-from ..domain.models import KeyboardMarkup, KeyboardButton
+from services.bot.domain.ports.output import (
+    LocalePort,
+    KeyboardProviderPort,
+    StoragePort
+)
+from services.bot.domain.models import KeyboardMarkup, KeyboardButton
+
+from aioboto3 import Session
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 
 class JSONLocaleProvider(LocalePort):
@@ -49,3 +58,31 @@ class KeyboardProvider(KeyboardProviderPort):
             )
 
         return KeyboardMarkup(type="reply", buttons=[kb_buttons])
+
+
+class S3Config(BaseSettings):
+    s3_access_key_id: str = Field()
+    s3_secret_access_key: str = Field()
+    s3_url: str = Field()
+
+    s3_region: Optional[str] = Field(default="us-east-1")
+    s3_session_key: Optional[str] = Field(default=None)
+
+
+class S3StoragePort(StoragePort):
+    config: S3Config
+    bucket: str = "audio-raw"
+
+    def __init__(self, config: S3Config):
+        self.config = config
+
+    async def upload_audio(self, filename: str, audio: BytesIO):
+        sess = Session(
+            aws_access_key_id=self.config.s3_access_key_id,
+            aws_secret_access_key=self.config.s3_secret_access_key,
+            aws_session_token=self.config.s3_session_key,
+            region_name=self.config.s3_region,
+        )
+
+        async with sess.client("s3", endpoint_url=self.config.s3_url) as s3:
+            await s3.upload_fileobj(audio, self.bucket, filename)
