d933dd9ff039850e41c1d2788cbfda4a10996ce7
Kirillkriv <kirill.krivoy@icloud.com>
2025-07-16 20:47:23 +0500

    fix - fixed import paths

--- services/bot/adapters/__init__.py
+++ services/bot/adapters/__init__.py
@@ -1,5 +1,6 @@
-from postgres_orm import PostgresConfig, PostgresORMRepository
-from json_locale import JSONLocaleProvider
-from keyboards import KeyboardProvider
-from kafka_mb import KafkaConfig, KafkaMessageBus
-from s3_storage import S3Config, S3StoragePort
+from .postgres_orm import PostgresConfig, PostgresORMRepository
+from .json_locale import JSONLocaleProvider
+from .keyboards import KeyboardProvider
+from .kafka_mb import KafkaConfig, KafkaMessageBus
+from .s3_storage import S3Config, S3StoragePort
+from .aiogram_botapi import AiogramBotAPI
