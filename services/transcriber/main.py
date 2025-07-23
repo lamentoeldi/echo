6e72ba35e81518c35c6151ae29ad10d01c9dfc02
Kirillkriv <kirill.krivoy@icloud.com>
2025-07-23 13:58:27 +0500

    feat - added models - added ports - added basic main

---
+++ services/transcriber/main.py
@@ -0,0 +1,9 @@
+import asyncio
+
+
+async def main():
+    # to be implemented
+    pass
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ services/transcriber/src/domain/models.py
@@ -0,0 +1,47 @@
+from uuid import UUID
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class ProcessingError(BaseModel):
+    code: int = Field()
+    message: str = Field()
+
+
+class MessageMeta(BaseModel):
+    status: str = Field()
+    error: Optional[ProcessingError] = None
+
+
+class AudioPreprocessed(BaseModel):
+    id: UUID = Field()
+    user_id: UUID = Field()
+    duration: int = Field()
+    size: int = Field()
+    channels: int = Field()
+    source: str = Field()
+    timestamp: int = Field()
+
+
+class AudioPreprocessedMessage(BaseModel):
+    """
+    Represents an AudioPreprocessed message
+    """
+    meta: MessageMeta = Field()
+    content: AudioPreprocessed = Field()
+
+
+class AudioTranscribed(BaseModel):
+    id: UUID = Field()
+    user_id: UUID = Field()
+    transcription: str = Field()
+    timestamp: int = Field()
+
+
+class AudioTranscribedMessage(BaseModel):
+    """
+    Represents an AudioTranscribedTg message
+    """
+    meta: MessageMeta = Field()
+    content: AudioTranscribed = Field()
