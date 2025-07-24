f21eeada50b2131229cc651388e0d4815d70f0f9
Kirillkriv <kirill.krivoy@icloud.com>
2025-07-24 19:50:32 +0500

    chore - refactored preprocessor: - moved setup_logger to infrastructure - separated adapters on packages by ports

--- services/preprocessor/main.py
+++ services/preprocessor/main.py
@@ -1,56 +1,14 @@
 import asyncio
-import logging
-import sys
-from logging import StreamHandler
 from signal import SIGINT, SIGTERM
 
-from src.adapters.kafka_controller.consumer import KafkaConsumerConfig, KafkaController
-from src.adapters.s3_storage import S3Config, S3StoragePort
-from src.adapters.kafka_message_bus import KafkaConfig, KafkaMessageBus
+from src.adapters.controllers import KafkaConsumerConfig, KafkaController
+from src.adapters.storage import S3Config, S3StoragePort
+from src.adapters.bus import KafkaConfig, KafkaMessageBus
 from src.application.usecases import PreprocessUseCase
 from src.domain.core import Core
 from src.infrastructure.metrics import MetricsServerConfig, MetricsServer
 from src.infrastructure.graceful_stop import GracefulStopper
-
-import structlog
-
-
-def setup_logger(
-    name: str = "main",
-    level: int = logging.INFO,
-    disable_other_loggers: bool = True
-) -> structlog.BoundLogger:
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    logger.propagate = False
-
-    handler = StreamHandler(sys.stdout)
-    handler.setLevel(level)
-    handler.setFormatter(logging.Formatter("%(message)s"))
-    logger.handlers = [handler]
-
-    structlog.configure(
-        processors=[
-            structlog.processors.TimeStamper(fmt="iso"),
-            structlog.stdlib.add_log_level,
-            structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
-            structlog.processors.JSONRenderer(),
-        ],
-        context_class=dict,
-        logger_factory=structlog.stdlib.LoggerFactory(),
-        wrapper_class=structlog.make_filtering_bound_logger(level),
-        cache_logger_on_first_use=True,
-    )
-
-    if disable_other_loggers:
-        for other_name in logging.root.manager.loggerDict:
-            if not other_name.startswith(name):
-                other_logger = logging.getLogger(other_name)
-                other_logger.setLevel(logging.CRITICAL + 1)
-                other_logger.propagate = False
-
-    return structlog.get_logger(name)
+from src.infrastructure.log import setup_logger
 
 
 async def main():
