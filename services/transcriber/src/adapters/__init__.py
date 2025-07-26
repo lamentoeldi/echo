1a61fa509fca6cecc30999709400bfc215462934
LaCoroutine <126378264+lamentoeldi@users.noreply.github.com>
2025-07-26 15:22:50 +0500

    fix - fixed transcriber paths - fixed transcriber dockerfile

--- services/transcriber/src/adapters/__init__.py
+++
@@ -1,4 +0,0 @@
-from .transcriber import WhisperConfig, WhisperAudioTranscriberAdapter
-from .s3_storage import S3Config, S3StoragePort
-from .kafka_mb import KafkaConfig, KafkaMessageBus
-from .kafka_consumer import KafkaConsumerConfig, KafkaController
